e8b402e46f15de99c9f8f68658d7b3da599342ca
Michelle Torres <chellet.jd@gmail.com>
2017-10-23 19:50:46 -0400

    Arreglos de codigo y html

--- js/artistas.js
+++ js/artistas.js
@@ -5,7 +5,7 @@ formBusqueda.addEventListener('submit', function (e) {
 	e.preventDefault();
 	let xhr = new XMLHttpRequest();
 	xhr.open('GET','https://api.spotify.com/v1/search?q=' + artista.value + '&type=artist',false);
-	xhr.setRequestHeader("Authorization",'Bearer BQBCYzS5iv5FxoEYkrQgWPelZuFff4Rh9jkZSC5ObtuiIwTi8TL9Z11dMcneBsF6LkByj6ehDLjV-LtUottp7pmkY4JPiSsr5P36kcYWYXEIVHcWCBoDVjUSqB9zmq1Z9PGRM8srAZssZy7nZIWFqpXIINiLiec')
+	xhr.setRequestHeader("Authorization",'Bearer BQDZPZxFXJLmAn8ZYRpiaqncXOqibGIbjBx2C9ZoDgTpOP58c_BPuRcoCPF3IT3gged5JrhxdQc-_4PeeZf3Lc6bVL6Ip8VT6JYjKkgeDA4bBoVIH_YtOwuJSZK85gBcaa18TqlEhpstarZElUTjZx0544xIZrE')
 	xhr.onload = () =>{
 		let json = JSON.parse(xhr.responseText).artists.items;
 		var artResult = document.querySelector('#artistasResult');
@@ -13,9 +13,9 @@ formBusqueda.addEventListener('submit', function (e) {
 		artista.value = "";
 		json.map(function (value) {
 			artResult.innerHTML += `
-			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 text-center">
-			<img class="img-responsive" src="${typeof(value.images[1]) != "undefined" ? value.images[1].url : '../img/notfound.jpg'}"/>
-			<p>${value.name}</p>
+			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 text-center">
+			<img class="img-responsive center-block imag" src="${typeof(value.images[1]) != "undefined" ? value.images[1].url : '../img/not-found.png'}"/>
+			<h3>${value.name}</h3>
 			</div>
 			`;
 		});
