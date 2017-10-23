fb4270d15d3604488bcd0ebde14f1d50e6fb9e53
Michelle Torres <chellet.jd@gmail.com>
2017-10-23 14:46:03 -0400

    Agregar imagenes en artistas.js

--- js/artistas.js
+++ js/artistas.js
@@ -1,9 +1,29 @@
-let xhr = new XMLHttpRequest();
+var formBusqueda = document.querySelector('#buscArt');
+var artista = document.querySelector('#artista');
 
-xhr.open('GET','https://api.spotify.com/v1/search?q=arjona&type=artist',false);
-xhr.setRequestHeader("Authorization",'Bearer BQBYvKbWtMuGm1Iemo831l1uG4FxJwn5DZF1PTKFvAfkxMH2kzS9EB8dzVsX80thzoV0xzzlg_Rwi7DvLzTOw068uaxHNEiGfCFYtw89d1cn2L_0Ai_b-Exz7C3YmAKJjYxAES30RpKYPjk-7PGlRo_kekR_5to')
-xhr.onload = () =>{
-console.log(xhr.responseText);
-}
+formBusqueda.addEventListener('submit', function (e) {
+	e.preventDefault();
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET','https://api.spotify.com/v1/search?q=' + artista.value + '&type=artist',false);
+	xhr.setRequestHeader("Authorization",'Bearer BQBCYzS5iv5FxoEYkrQgWPelZuFff4Rh9jkZSC5ObtuiIwTi8TL9Z11dMcneBsF6LkByj6ehDLjV-LtUottp7pmkY4JPiSsr5P36kcYWYXEIVHcWCBoDVjUSqB9zmq1Z9PGRM8srAZssZy7nZIWFqpXIINiLiec')
+	xhr.onload = () =>{
+		let json = JSON.parse(xhr.responseText).artists.items;
+		var artResult = document.querySelector('#artistasResult');
+		artResult.innerHTML = "";
+		artista.value = "";
+		json.map(function (value) {
+			artResult.innerHTML += `
+			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 text-center">
+			<img class="img-responsive" src="${typeof(value.images[1]) != "undefined" ? value.images[1].url : '../img/notfound.jpg'}"/>
+			<p>${value.name}</p>
+			</div>
+			`;
+		});
+	//console.log(xhr.responseText);
+	}
 
-xhr.send();
+	xhr.send();
+
+})
+
+
