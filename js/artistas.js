3ee36bdc11580926ef18498da8bacb60af9d13c8
chelletjd <chellet.jd@gmail.com>
2017-10-24 20:50:05 -0400

    Modificacion de token y agregar botones y modificaciones de busqueda

--- js/artistas.js
+++ js/artistas.js
@@ -5,7 +5,7 @@ formBusqueda.addEventListener('submit', function (e) {
 	e.preventDefault();
 	let xhr = new XMLHttpRequest();
 	xhr.open('GET','https://api.spotify.com/v1/search?q=' + artista.value + '&type=artist',false);
-	xhr.setRequestHeader("Authorization",'Bearer BQDZPZxFXJLmAn8ZYRpiaqncXOqibGIbjBx2C9ZoDgTpOP58c_BPuRcoCPF3IT3gged5JrhxdQc-_4PeeZf3Lc6bVL6Ip8VT6JYjKkgeDA4bBoVIH_YtOwuJSZK85gBcaa18TqlEhpstarZElUTjZx0544xIZrE')
+	xhr.setRequestHeader("Authorization",'Bearer BQAQXgngnjqiN7V12OYG29707gLYjtNKE1qknYe4qbfKB7CgYkAN2ulRmPuYIe5RG93ZkCgiBYMsPwNCk1OQO5I6KMa0fMsjzm-FdfJK5GCGkWiPtMsoX11EhC6kRUuQKXYc_F3Tr1hg1Pc3APCcNTKIct5SX48')
 	xhr.onload = () =>{
 		let json = JSON.parse(xhr.responseText).artists.items;
 		var artResult = document.querySelector('#artistasResult');
@@ -14,7 +14,7 @@ formBusqueda.addEventListener('submit', function (e) {
 		json.map(function (value) {
 			artResult.innerHTML += `
 			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 text-center">
-			<img class="img-responsive center-block imag" src="${typeof(value.images[1]) != "undefined" ? value.images[1].url : '../img/not-found.png'}"/>
+			<a data-toggle="modal" data-target="#myModal"><img cursor="pointer" class="img-responsive center-block imag" src="${typeof(value.images[1]) != "undefined" ? value.images[1].url : '../img/not-found.png'}"/></a>
 			<h3>${value.name}</h3>
 			</div>
 			`;
--- js/index.js
+++ js/index.js
@@ -1,21 +1,28 @@
-var client_id = '509dbc14e3454368ab55705e72429a68'; // Tu Client ID
-var redirect_uri = 'http://localhost:3000/artistas.html'; // Tu Redirect URI (Direccion URL a donde quieres que redireccione luego de iniciar Sesion Con tu cuenta de Spotify.)
-var state = generateRandomString(16);
-localStorage.setItem('spotify', state);
-var scope = 'user-read-private user-read-email';
-var url = 'https://accounts.spotify.com/authorize';
-url += '?response_type=token';
-url += '&client_id=' + encodeURIComponent(client_id);
-url += '&scope=' + encodeURIComponent(scope);
-url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-url += '&state=' + encodeURIComponent(state);
-window.location = (url);
+var active = document.querySelector('#activar');
 
-function generateRandomString(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
+
+active.addEventListener('click', function (even) {
+	even.preventDefault();
+
+	var client_id = '509dbc14e3454368ab55705e72429a68'; // Tu Client ID
+	var redirect_uri = 'http://localhost:3000/artistas.html'; // Tu Redirect URI (Direccion URL a donde quieres que redireccione luego de iniciar Sesion Con tu cuenta de Spotify.)
+	var state = generateRandomString(16);
+	localStorage.setItem('spotify', state);
+	var scope = 'user-read-private user-read-email';
+	var url = 'https://accounts.spotify.com/authorize';
+	url += '?response_type=token';
+	url += '&client_id=' + encodeURIComponent(client_id);
+	url += '&scope=' + encodeURIComponent(scope);
+	url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+	url += '&state=' + encodeURIComponent(state);
+	window.location = (url);
+
+	function generateRandomString(length) {
+	  var text = '';
+	  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	  for (var i = 0; i < length; i++) {
+	    text += possible.charAt(Math.floor(Math.random() * possible.length));
+	  }
+	  return text;
+	};
+});
