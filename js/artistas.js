89a87a32db84e4f65f0a60bbf6143ff25bf67ac7
chelletjd <chellet.jd@gmail.com>
2017-10-25 23:18:43 -0400

    Guarda token en localStorage y agregar datos a modals

--- js/artistas.js
+++ js/artistas.js
@@ -1,8 +1,15 @@
+if(getHashParams().access_token){
+   localStorage.setItem("token",getHashParams().access_token);
+   window.location = "artistas.html";
+}
+
+var token = localStorage.getItem("token");
 var formBusqueda = document.querySelector('#buscArt');
 var artista = document.querySelector('#artista');
 var tipo = document.querySelector('#selector');
 var artName;
-var token = 'BQAqTEOybuleL00ghJqSFZPP38GzF7lCno4qUcCQ2baOtJ5D_r4kk-jr3wNFZqCnXpBqakB37gVU4htAyWw5MBiX2pnfOFOG633CCJX6hGrXVKcYSRjCu2eWzNvyU9woTeO9xilNf0xsLK-yzYvCfMTfyqddhuk'
+// var token = getHashParams().access_token;
+console.log(token);
 formBusqueda.addEventListener('submit', function (e) {
 	e.preventDefault();
 	let xhr = new XMLHttpRequest();
@@ -19,32 +26,36 @@ formBusqueda.addEventListener('submit', function (e) {
 				console.log("value  " + value.id);
 				artResult.innerHTML += `
 				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 text-center">
-				<a data-toggle="modal" data-target="#myModal"><img id="${value.id}" class="img-responsive center-block imag" src="${typeof(value.images[1]) != "undefined" ? value.images[1].url : '../img/not-found.png'}"/></a>
+				<a data-toggle="modal" data-target="#myModal"><img name="${value.name}" id="${value.id}" class="img-responsive center-block imag" src="${typeof(value.images[1]) != "undefined" ? value.images[1].url : '../img/not-found.png'}"/></a>
 				<h3>${value.name}</h3>
 				</div>
-				`;
-
-				artName = value.name;
-
-
+				`; 
 				setTimeout(function(){
 					document.getElementById(value.id).parentElement.addEventListener('click', function (arg) {
 						let xh = new XMLHttpRequest();
+						var name = arg.target.name;
+						console.log(name);
 						xh.open('GET',`https://api.spotify.com/v1/artists/${arg.target.id}/albums` ,true);
 						xh.setRequestHeader("Authorization",'Bearer '+ token);
 						xh.onload = () => {
 							let jso = JSON.parse(xh.responseText).items;
-							console.log(jso);
+							console.log('jso  '+jso);
 							var modalHeader = document.querySelector('#modalTitle');
-
-							modalHeader.innerHTML = artName;
-							// jso.map(function (ar) {
-							// 	if (ar.album_type = 'album') {
-							// 		modalHeader.innerHTML = ar.artists[0].name;
-							// 	}
-								
-
-							// });
+							var modalBody = document.querySelector('#modalBody');
+							modalHeader.innerHTML = "";
+							modalBody.innerHTML = "";
+							modalHeader.innerHTML = name + " - Albums";
+							jso.map(function (a) {
+									if (a.album_type == 'album') {console.log('album')
+									console.log("aaa  " + a.id);
+									modalBody.innerHTML += `
+									<img name="${a.name}" id="${a.id}" class="img-responsive center-block imag-sm" src="${typeof(a.images[1]) != "undefined" ? a.images[1].url : '../img/not-found.png'}"/></a>
+									<h5>${a.name}</h5>
+									</div>
+									`; 
+								}
+							});
+											
 						}
 						xh.send();
 					});
@@ -59,27 +70,34 @@ formBusqueda.addEventListener('submit', function (e) {
 			json.map(function (value) {
 				artResult.innerHTML += `
 				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 text-center">
-				<a data-toggle="modal" data-target="#myModal"><img class="img-responsive center-block imag" src="${typeof(value.images[1]) != "undefined" ? value.images[1].url : '../img/not-found.png'}"/></a>
+				<a data-toggle="modal" data-target="#myModal"><img name="${value.name}" id="${value.id}" class="img-responsive center-block imag" src="${typeof(value.images[1]) != "undefined" ? value.images[1].url : '../img/not-found.png'}"/></a>
 				<h3>${value.name}</h3>
 				</div>
 				`;
-			});
-
-		}else if (tipo.value == 'track') {
-			let json = JSON.parse(xhr.responseText).tracks.items;
-			console.log(json);
-			var artResult = document.querySelector('#artistasResult');
-			artResult.innerHTML = "";
-			artista.value = "";
-			json.map(function (value) {
-				console.log("value   " + value.images[1]);
-
-				artResult.innerHTML += `
-				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 text-center">
-				<a data-toggle="modal" data-target="#myModal"><img class="img-responsive center-block imag" src="${typeof(value.images[1]) != "undefined" ? value.images[1].url : '../img/not-found.png'}"/></a>
-				<h3>${value.name}</h3>
-				</div>
-				`;
+				setTimeout(function(){
+					document.getElementById(value.id).parentElement.addEventListener('click', function (arg) {
+						let xh = new XMLHttpRequest();
+						var name = arg.target.name;
+						console.log(name);
+						xh.open('GET',`https://api.spotify.com/v1/albums/${arg.target.id}/tracks` ,true);
+						xh.setRequestHeader("Authorization",'Bearer '+ token);
+						xh.onload = () => {
+							let jso = JSON.parse(xh.responseText).items;
+							console.log(jso);
+							var modalHeader = document.querySelector('#modalTitle');
+							var modalBody = document.querySelector('#modalBody');
+							modalHeader.innerHTML = name + " - Canciones";
+							jso.map(function (a) {
+								console.log("aaa  " + a.id);
+								modalBody.innerHTML += `
+								<a href="${a.preview_url}"><h5>${a.name}</h5></a>
+								`; 
+							});
+											
+						}
+						xh.send();
+					});
+				}, 0);
 			});
 
 		}else{
@@ -88,3 +106,13 @@ formBusqueda.addEventListener('submit', function (e) {
 	}
 	xhr.send();
 });
+
+function getHashParams() {
+  var hashParams = {};
+  var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+  while ( e = r.exec(q)) {
+     hashParams[e[1]] = decodeURIComponent(e[2]);
+  }
+  return hashParams;
+}
